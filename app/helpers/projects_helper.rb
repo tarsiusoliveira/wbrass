7a1b39820d23536bb6b5bf1de09676e785ca506e
Tarsius Oliveira <tarsiusoliveira@gmail.com>
2022-05-01 17:10:07 -0300

    Introduces text file creation to any created projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,30 @@
 module ProjectsHelper
+    class FalseClass; def to_i; 0 end end
+    class TrueClass; def to_i; 1 end end
+
+    def txtFileMaker(project)
+
+        filename = "Project_#{project.id}"
+        File.open(filename, File::WRONLY|File::CREAT) do |f|
+            f.write("<ENVIRONMENT>\n")
+            f.write("TEMPERATURE =          #{project.temperature}\n")
+            f.write("PRESSURE =             #{project.pressure}\n")
+            f.write("HUMIDITY =             #{project.pressure}\n")
+            f.write("</ENVIRONMENT>\n\n")
+            f.write("<SIMULATION>\n")
+            f.write("MAX_TIME =             #{project.irDuration}\n")
+            f.write("STOP_DECAY =           #{project.energyDecay}\n")
+            f.write("SAMPLE_RATE =          #{project.sampleRate}\n")
+            f.write("CLUSTER_ORDER =        #{project.clusterOrder}\n")
+            f.write("MAX_RAYTRAC_REF =      #{project.maxReflections}\n")
+            f.write("SAVE_LATE_RAYS =       #{project.saveLateRays ? 1 : 0}\n")
+            f.write("DIFFUSE_PROCESSING =   #{project.diffuseProcessing ? 1 : 0}\n")
+            f.write("SAVE_DIFFUSE_RAYS =    #{project.saveDiffuseRays ? 1 : 0}\n")
+            f.write("ARTIFICIAL_TAIL =      #{project.artificialTail ? 1 : 0}\n")
+            f.write("AURALIZATION =         #{project.auralization ? 1 : 0}\n")
+            f.write("</SIMULATION>\n\n")
+            f.write("<LAYERS::SOURCE>")
+            f.close()
+        end      
+    end
 end
